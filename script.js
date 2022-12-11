6ef561eb7497106e628d6dd8cc1a78aaf1e2fbe1
John Doe <jack.h.sawyer@pm.me>
2022-12-11 08:57:46 -0800

    function updates

--- script.js
+++ script.js
@@ -1,17 +1,19 @@
 function dochange() {
-    let text = "clicked Cancel button with JavaScript function";
+    let text = "Go ahead and pick one!";
     if (confirm(text) == true) {
-      text = "clicked OK button with JavaScript function"
+      text = "You clicked OK and got this JavaScript function."
+    } else {
+      text = "You clicked CANCEL and got a different JavaScript function."
     }
     document.getElementById("demo").innerHTML = text;
   }
   
   function dochange2() {
-      let text = "Choose WISELY!\nOkay or Nope.";
+      let text = "Choose WISELY!\nOkay or Cancel.";
     if (confirm(text) == true) {
-      text = "Bye.";
+      text = "You have chosen WISELY!";
     } else {
-      text = "Pfft!";
+      text = "You have chosen POORLY!";
     }
     document.getElementById("demo").innerHTML = text;
   }
